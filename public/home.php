4a50a4aa2ff6090ab4b1658da99b1c88991796fe
Henry Barnes <henrytbarnes07@gmail.com>
2025-07-29 14:46:30 +0100

    added basic quill for text editor

--- public/home.php
+++ public/home.php
@@ -12,8 +12,6 @@ if (!$page = $ReadPages->getPage(1)) {
     Utility::redirect('http://notion.test/error');
 }
 
-Utility::inspectAndDie($page);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,18 +20,33 @@ Utility::inspectAndDie($page);
     <meta charset="UTF-8">
     <title>Henry's NO-tion</title>
     <link href="./public/css/output.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css" rel="stylesheet" />
 </head>
 
 <body>
-    <div id="content" class='w-screen h-screen bg-red-300 flex'>
+    <div id="content" class='w-screen h-screen flex'>
         <div id="navigationSection" class='h-full w-1/8 bg-green-300'>
 
         </div>
 
-        <div id="contentSection" class='h-full w-7/8 bg-blue-300'>
-
+        <div id="contentSection" class='h-full w-7/8'>
+            <div id="editor">
+                <p>Hello World!</p>
+                <p>Some initial <strong>bold</strong> text</p>
+                <p><br /></p>
+            </div>
         </div>
+
+        <!-- Include the Quill library -->
+        <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
     </div>
+
+    <!-- Initialize Quill editor -->
+    <script>
+        const quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+    </script>
 </body>
 
 </html>
